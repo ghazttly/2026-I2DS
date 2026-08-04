e7727271c4e526d45b02e3fb9ddd834214e40262
ghazttly <yasminviaro7@gmail.com>
2026-08-04 16:57:28 -0300

    exercício 01 concluído exercício 02 concluído

--- Aula-03/Exercicios/Ex-01/Program.cs
+++ Aula-03/Exercicios/Ex-01/Program.cs
@@ -2,6 +2,12 @@
 {
     private static void Main(string[] args)
     {
-        
+        int celsius;
+        Console.Write("Digite a temperatura em celsius: ");
+        celsius = int.Parse(Console.ReadLine());
+
+        double fahreinheit = (celsius * 9 / 5) + 32;
+
+        Console.WriteLine("A temperatura em fahreinheit é: " + fahreinheit);
     }
 }
--- Aula-03/Exercicios/Ex-02/Program.cs
+++ Aula-03/Exercicios/Ex-02/Program.cs
@@ -4,6 +4,45 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.Write("Digite um numero");
+        int num1;
+        int num2;
+
+        Console.Write("Digite um numero: ");
+        num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite outro numero: ");
+        num2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("1 - Somar");
+        Console.WriteLine("2 - Subtrair");
+        Console.WriteLine("3 - Multiplicar");
+        Console.WriteLine("4 - Dividir");
+
+        string operacao;
+
+        Console.Write("Selecione uma operacao: ");
+        operacao = Console.ReadLine();
+
+        switch (operacao)
+        {
+            case "1": 
+                Console.WriteLine(num1 + num2);
+                break;
+            case "2": 
+                Console.WriteLine(num1 - num2);
+                
+                break;
+            case "3": 
+                Console.WriteLine(num1 * num2);
+                break;
+            case "4": 
+                Console.WriteLine(num1 / num2);
+                break;
+            default:
+                Console.WriteLine("Opçao inválida");
+                break;
+        }
+
+        
     }
 }

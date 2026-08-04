6fb14452062942f8e63231aa65f426b35ad59d85
ghazttly <yasminviaro7@gmail.com>
2026-08-04 15:28:11 -0300

    Finalizado a Aula-03

---
+++ Aula-03/Funcao/Program.cs
@@ -0,0 +1,54 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       Ola("Cristiano");
+        Ola("Maria");
+        Ola("João");
+
+        Console.WriteLine("A soma de 5 e 3 é: " + Calcular(5,3,"+"));
+        Console.WriteLine("A subtraçao de 5 e 3 é: " + Calcular(5,3,"-"));
+        Console.WriteLine("A soma de 5 e 3 é: " + Calcular(5,3,"#"));
+
+
+    }
+
+    /* public static void Ola()
+    {
+      Console.WriteLine("Ola, Seja bem vindo!");  
+    }
+    */
+
+    // Funçao com parametro nome e sem retorno
+
+    public static void Ola(string nome)
+    {
+        Console.WriteLine("Olá " + nome + ", Seja bem vindo(a)!");
+    }
+
+    public static double Calcular(double num1, double num2, string operador)
+    {
+        double resultado=0;
+
+        switch (operador)
+        {
+            case "+":
+                resultado = num1 + num2;
+                break;
+            case "-":
+                resultado = num1 - num2;
+                break;
+            case "*":
+                resultado = num1 * num2;
+                break;
+            case "/":
+                resultado = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("Operador informado nao é valido");
+                break;
+        }
+
+        return resultado;
+    }
+}

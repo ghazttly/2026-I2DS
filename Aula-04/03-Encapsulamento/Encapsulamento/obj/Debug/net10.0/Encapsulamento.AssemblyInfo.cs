f58868ec8bdd7263a41d141491e3cac02d44be42
ghazttly <yasminviaro7@gmail.com>
2026-08-11 13:46:46 -0300

    lista de exercícios

--- Aula-04/03-Encapsulamento/Encapsulamento/obj/Debug/net10.0/Encapsulamento.AssemblyInfo.cs
+++ Aula-04/03-Encapsulamento/Encapsulamento/obj/Debug/net10.0/Encapsulamento.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Encapsulamento")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40b6bede37de68391d8bd97ba30b62bc545adaba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3ad2e7a87a0cee4d98717d94efcb755b6684849")]
 [assembly: System.Reflection.AssemblyProductAttribute("Encapsulamento")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Encapsulamento")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
